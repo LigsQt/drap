d88cd1d547a983495bff948e128700f66345fd26
Eriene Galinato <edgalinato@up.edu.ph>
2026-03-29 23:16:32 +0800

    feat(drafts/system-logs): add loader for faculty choices

---
+++ src/lib/features/drafts/system-logs/loader.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import Loader2Icon from '@lucide/svelte/icons/loader-2';
+
+  import Empty from '$lib/components/ui/empty/empty.svelte';
+  import { createFetchDraftFacultyChoicesQuery } from '$lib/queries/fetch-draft-faculty-choices';
+
+  export interface Props {
+    draftId: string;
+  }
+
+  const { draftId }: Props = $props();
+
+  const query = $derived(createFetchDraftFacultyChoicesQuery(draftId));
+</script>
+
+{#if query.isPending}
+  <div class="flex h-full items-center justify-center">
+    <Loader2Icon class="size-20 animate-spin" />
+  </div>
+{:else if query.isError}
+  <Empty>Uh oh! An error has occurred.</Empty>
+{:else}
+{/if}
