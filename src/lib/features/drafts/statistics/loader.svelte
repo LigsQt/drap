ab22b94d123270946d4c1cc544f4e50e01d2e809
Eriene Galinato <edgalinato@up.edu.ph>
2026-04-02 14:45:49 +0800

    feat(drafts/statistics): make loader component

---
+++ src/lib/features/drafts/statistics/loader.svelte
@@ -0,0 +1,47 @@
+<script lang="ts" module>
+  import type { ExternalProps as DisplayProps } from './display.svelte';
+
+  export interface Props extends DisplayProps {
+    draftId: string;
+  }
+</script>
+
+<script lang="ts">
+  import Loader2Icon from '@lucide/svelte/icons/loader-2';
+
+  import Display from './display.svelte';
+  import Empty from '$lib/components/ui/empty/empty.svelte';
+  import { createFetchDraftAssignmentsQuery } from '$lib/queries/fetch-draft-assignments';
+  import { createFetchDrafteesQuery } from '$lib/queries/fetch-draftees';
+
+  const { draftId, ...displayProps }: Props = $props();
+  const { maxRounds } = $derived(displayProps);
+
+  const regularDraftedQuery = $derived(
+    createFetchDraftAssignmentsQuery(draftId, assignments =>
+      assignments.filter(({ round }) => round !== null && round > 0 && round <= maxRounds),
+    ),
+  );
+  const interventionDraftedQuery = $derived(
+    createFetchDraftAssignmentsQuery(draftId, assignments =>
+      assignments.filter(({ round }) => round !== null && round === maxRounds + 1),
+    ),
+  );
+  const lotteryDraftedQuery = $derived(
+    createFetchDraftAssignmentsQuery(draftId, assignments =>
+      assignments.filter(({ round }) => round === null),
+    ),
+  );
+
+  const drafteesQuery = $derived(createFetchDrafteesQuery(draftId));
+</script>
+  
+{#if regularDraftedQuery.isError || interventionDraftedQuery.isError || lotteryDraftedQuery.isError || drafteesQuery.isError}
+  <Empty>Uh oh! An error has occurred.</Empty>
+{:else if regularDraftedQuery.isPending || interventionDraftedQuery.isPending || lotteryDraftedQuery.isPending || drafteesQuery.isPending}
+  <div class="flex h-full items-center justify-center">
+    <Loader2Icon class="size-20 animate-spin" />
+  </div>
+{:else}
+  <Display regularDrafted={regularDraftedQuery.data} interventionDrafted={interventionDraftedQuery.data} lotteryDrafted={lotteryDraftedQuery.data} {...displayProps} />
+{/if}
