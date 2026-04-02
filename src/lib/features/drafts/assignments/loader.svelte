6e8b8d7aaf301264a1b0eae03c5d077edd023859
Eriene Galinato <edgalinato@up.edu.ph>
2026-04-02 16:58:07 +0800

    feat(drafts/assignments): make loader component

---
+++ src/lib/features/drafts/assignments/loader.svelte
@@ -0,0 +1,42 @@
+<script lang="ts" module> 
+  export interface Props {
+    draftId: string;
+    maxRounds: number;
+  }
+</script>
+
+<script lang="ts">
+  import Loader2Icon from '@lucide/svelte/icons/loader-2';
+
+  import Display from './display.svelte';
+  import Empty from '$lib/components/ui/empty/empty.svelte';
+  import { createFetchDraftAssignmentsQuery } from '$lib/queries/fetch-draft-assignments';
+
+  const { draftId, maxRounds }: Props = $props();
+
+  const regularDraftedQuery = $derived(
+    createFetchDraftAssignmentsQuery(draftId, assignments =>
+      assignments.filter(({ round }) => round !== null && round > 0 && round <= maxRounds),
+    ),
+  );
+  const interventionDraftedQuery = $derived(
+    createFetchDraftAssignmentsQuery(draftId, assignments =>
+      assignments.filter(({ round }) => round !== null && round === maxRounds + 1),
+    ),
+  );
+  const lotteryDraftedQuery = $derived(
+    createFetchDraftAssignmentsQuery(draftId, assignments =>
+      assignments.filter(({ round }) => round === null),
+    ),
+  );
+</script>
+  
+{#if regularDraftedQuery.isError || interventionDraftedQuery.isError || lotteryDraftedQuery.isError}
+  <Empty>Uh oh! An error has occurred.</Empty>
+{:else if regularDraftedQuery.isPending || interventionDraftedQuery.isPending || lotteryDraftedQuery.isPending}
+  <div class="flex h-full items-center justify-center">
+    <Loader2Icon class="size-20 animate-spin" />
+  </div>
+{:else}
+  <Display regularDrafted={regularDraftedQuery.data} interventionDrafted={interventionDraftedQuery.data} lotteryDrafted={lotteryDraftedQuery.data} />
+{/if}
